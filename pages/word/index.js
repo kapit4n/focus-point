c6afb3d981842eaaf0a57994f1b36e2efd01f9c6
Luis Arce <luis.arce22@gmail.com>
2022-10-11 22:08:32 -0400

    Word movement

---
+++ pages/word/index.js
@@ -0,0 +1,38 @@
+import styles from '../../styles/word.module.css'
+import classNames from 'classnames';
+import { useState, useEffect } from 'react';
+
+export default function Index({ word, size }) {
+
+  const [leftPosition, setLeftPosition] = useState(0)
+  const [topPosition, setTopPosition] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(function () {
+      console.log(leftPosition)
+      console.log(topPosition)
+
+      setLeftPosition(leftPosition + 100)
+      setTopPosition(topPosition + 100)
+    }, 1000)
+    
+    return () => {
+      console.log('cler interal')
+      clearInterval(interval)
+    }
+  }, [])
+
+  return (
+    <div style={{ left: leftPosition, top: topPosition, backgroundColor: 'blue' }}>
+      <span className={classNames(styles.word, styles[size])}>
+        {word}
+      </span>
+
+    </div>
+  )
+}
+
+Index.defaultProps = {
+  word: 'this is the test',
+  size: 'small'
+}
